d00612edba30a1a9dd37478c353ef857aa4186f4
matheus zanetti <matheus.zanetti@shipp.delivery>
2019-06-27 19:57:51 -0300

    deploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -16,7 +16,7 @@ app.get('/api/hello', (req, res) => {
 app.post('/api/world', (req, res) => {
   console.log(req.body);
   res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
+      `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
 
